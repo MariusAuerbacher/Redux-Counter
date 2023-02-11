40955f1dcaad5d82cb6556bd8350ac8ea8edfddd
MariusAuerbacher <101823468+MariusAuerbacher@users.noreply.github.com>
2023-02-11 17:10:00 +0100

    upgraded to reduxjs/toolkit and createSlice

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,30 @@
-import {configureStore} from '@reduxjs/toolkit'
+import {configureStore, createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
   counter: 0,
   showCounter: true
 }
 
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState: initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter
+    }
+  }
+})
+
+/*
 const counterReducer = (state = initialState, action) => {
 
   if(action.type === 'increment'){
@@ -42,6 +62,14 @@ const counterReducer = (state = initialState, action) => {
 
 const store = configureStore({
   reducer: counterReducer
-})
+})*/
+
+
+
+const store = configureStore({
+  reducer: counterSlice.reducer
+}) 
+
+export const counterActions = counterSlice.actions
 
 export default store
