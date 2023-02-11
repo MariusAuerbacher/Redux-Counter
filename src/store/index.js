e234303a9d8e3cb5f2c80810e48095c0fea2a5ba
MariusAuerbacher <101823468+MariusAuerbacher@users.noreply.github.com>
2023-02-11 12:27:40 +0100

    added a toggle and an increment by 5

--- src/store/index.js
+++ src/store/index.js
@@ -2,19 +2,38 @@ import {configureStore} from '@reduxjs/toolkit'
 
 const initialState = {
   counter: 0,
+  showCounter: true
 }
 
 const counterReducer = (state = initialState, action) => {
 
   if(action.type === 'increment'){
     return {
-      counter: state.counter + 1
+      ...initialState,
+      counter: state.counter + 1,
+      showCounter: state.showCounter
+    
     }
   }
-
+  if(action.type === 'increase'){
+    return{
+      ...initialState,
+      counter: state.counter + action.amount,
+      showCounter: state.showCounter
+    }
+  }
   if(action.type === 'decrement'){
     return {
-      counter: state.counter - 1
+      ...initialState,
+      counter: state.counter - 1,
+      showCounter: state.showCounter
+    }
+  }
+  if(action.type === 'toggle'){
+    return{
+      ...initialState,
+      counter: state.counter,
+      showCounter: !state.showCounter
     }
   }
 
